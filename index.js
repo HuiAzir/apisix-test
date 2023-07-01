088cf3b8760f5f6ef93f49146cbac27ce3891ffd
HuiAzir <huiazir@qq.com>
2023-07-01 11:25:03 +0800

    fit

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ router.get('/', (ctx, next) => {
   ctx.body = '首页';
 });
 
+router.get('/test', (ctx, next) => {
+  // ctx.router available
+  ctx.body = 'hello word';
+});
+
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4122);
